50188f88e9d8feabb9a1512206fc32ca24d517fa
Ceesvanmook <62257323+Ceesvanmook@users.noreply.github.com>
2023-07-06 11:25:18 +0200

    Update urls.py

--- django_drf_filepond/urls.py
+++ django_drf_filepond/urls.py
@@ -4,16 +4,16 @@ Based on the server-side configuration details
 provided at:
 https://pqina.nl/filepond/docs/patterns/api/server/#configuration
 """
-from django.conf.urls import url
+from django.urls import path, re_path, include
 from django_drf_filepond.views import ProcessView, RevertView, LoadView,\
      RestoreView, FetchView, PatchView
 
 urlpatterns = [
-    url(r'^process/$', ProcessView.as_view(), name='process'),
-    url(r'^patch/(?P<chunk_id>[0-9a-zA-Z]{22})$', PatchView.as_view(),
+    re_path(r'^process/$', ProcessView.as_view(), name='process'),
+    re_path(r'^patch/(?P<chunk_id>[0-9a-zA-Z]{22})$', PatchView.as_view(),
         name='patch'),
-    url(r'^revert/$', RevertView.as_view(), name='revert'),
-    url(r'^load/$', LoadView.as_view(), name='load'),
-    url(r'^restore/$', RestoreView.as_view(), name='restore'),
-    url(r'^fetch/$', FetchView.as_view(), name='fetch')
+    re_path(r'^revert/$', RevertView.as_view(), name='revert'),
+    re_path(r'^load/$', LoadView.as_view(), name='load'),
+    re_path(r'^restore/$', RestoreView.as_view(), name='restore'),
+    re_path(r'^fetch/$', FetchView.as_view(), name='fetch')
 ]
